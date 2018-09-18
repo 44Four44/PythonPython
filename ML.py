3473d60dc4a7ba142956c3bca9b684dda697a213
Yu Zhang <EthanWang@Yus-MacBook-Air.local>
2018-09-18 12:50:08 -0400

    h

---
+++ ML.py
@@ -0,0 +1,7 @@
+import numpy as np
+from sklearn import tree
+
+features =
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit()
--- pi.py
+++ pi.py
@@ -1,12 +1,12 @@
 import random
 
-#setup
+# setup
 accuracy = 100000.0
 
 inCircle = 0.0
 
-for i in range (1, int(accuracy)):
-    if (random.uniform(0, 1)**2 + random.uniform(0, 1)**2)**0.5 < 1:
-        inCircle+=1
+for i in range(1, int(accuracy)):
+    if (random.uniform(0, 1) ** 2 + random.uniform(0, 1) ** 2) ** 0.5 < 1:
+        inCircle += 1
 
-print(4*inCircle/accuracy)
+print(4 * inCircle / accuracy)
