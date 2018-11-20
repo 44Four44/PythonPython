46be46bc264358e92b9dda59e7a19e88fa627650
Yu Zhang <EthanWang@Yus-MacBook-Air.local>
2018-11-20 10:39:38 -0500

    manual input for first letter of word

--- Shiritori Bot/main.py
+++ Shiritori Bot/main.py
@@ -1,21 +1,57 @@
 from pynput import keyboard
 from pynput.keyboard import Key, Controller
 
-
+run = True
 board = Controller()
 
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+           'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-board.type("ASDsadas")
+# The dictionary
+words = [[]]
+# Counts words from each letter so words aren't resused
+count = [0]
+
+# Read words
+with open('/Users/EthanWang/Python/Shiritori Bot/words.txt', 'r') as file:
+    data = file.readlines()
+    i = 0
+    for line in data:
+        if line == '\n':
+            i += 1
+            words.append([])
+            count.append(0)
+        else:
+            words[i].append(line.rstrip())
+
 def on_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
 
+    if key.char == '1':
+        board.press(Key.cmd)
+        board.press('a')
+        board.release('a')
+        board.release(Key.cmd)
+    else:
+        n = letters.index(key.char)
 
+        if count[n] < len(words[n]):
+            board.press(Key.backspace)
+            board.release(Key.backspace)
+
+            board.type(words[n][count[n]])
+
+            count[n] += 1
+
+            board.press(Key.enter)
+            board.release(Key.enter)
+        else:
+            print("No more words left")
+
+    return False
 
 # Collect events until released
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
+while run:
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+
+
